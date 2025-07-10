eb309182a7cdcb51a096d03a1b5db58a073d8ee3
sonihidayatulloh797 <sonihidayatulloh797@gmail.com>
2025-07-10 12:50:14 +0700

    Update dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,19 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        if(auth()->user()->role_display()=="Admin"){
+        if (auth()->user()->role_display() == "Admin") {
+            // Diagnosa terbanyak
+            $dataDiagnosaTerbanyak = DB::select("
+                SELECT aa.diagnosa, aa.total, ic.name_id
+                FROM (
+                    SELECT sc.diagnosa AS diagnosa, COUNT(sc.diagnosa) AS total
+                    FROM (
+                        SELECT a.diagnosa
+                        FROM rekam_diagnosa a
+                        LEFT JOIN rekam r ON r.id = a.rekam_id
+                        WHERE a.diagnosa IS NOT NULL AND r.tgl_rekam LIKE '%2025-%'
 
-            return view('dashboard.admin');
-        }else if(auth()->user()->role_display()=="Pendaftaran"){
+                        UNION ALL
+
+                        SELECT rg.diagnosa
+                        FROM rekam_gigi rg
+                        WHERE rg.created_at LIKE '%2025-%'
+                    ) sc
+                    GROUP BY sc.diagnosa
+                ) aa
+                LEFT JOIN icds ic ON ic.code = aa.diagnosa
+                ORDER BY aa.total DESC
+                LIMIT 10
+            ");
+
+            return view('dashboard.admin', compact('dataDiagnosaTerbanyak'));
+        }
+
+        if (auth()->user()->role_display() == "Pendaftaran") {
             return view('dashboard.registrasi');
-        }else if(auth()->user()->role_display()=="Dokter"){
+        }
+
+        if (auth()->user()->role_display() == "Dokter") {
             return view('dashboard.dokter');
-        }else if(auth()->user()->role_display()=="Apotek"){
+        }
+
+        if (auth()->user()->role_display() == "Apotek") {
             return view('dashboard.obat');
         }
     }
